5e1e1ec2a33ba0182ba92f9d44f3cbc2c71d7d2c
Khanh Do Quoc <dqkhanh2@tma.com.vn>
2022-09-16 15:27:27 +0700

    moveZero

--- array/moveZero.js
+++ array/moveZero.js
@@ -13,5 +13,20 @@ var moveZeroes = function(nums) {
     return [...nums, ...temp]
 };
 
+var moveZeroeswithoutneedtemp = function(nums) {
+    let index =0;
+    for(let i = 0; i < nums.length; i++){
+        const num = nums[i]
+        if(num !==0){
+            nums[index] = num;
+            index++
+        }
+    }
+    for(let i = index; i< nums.length; i++){
+        nums[i] = 0
+    }
+    return nums
+};
+
 const arr = [0,1,0,3,12]; //[ 1, 3, 12, 0, 0 ]
-console.log(moveZeroes(arr))
+console.log(moveZeroeswithoutneedtemp(arr))
